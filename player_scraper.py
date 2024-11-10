50c9bbc3f4507c23839c4470e32da5fbf0e07502
Arjun Ranade <arjunranade@unknown4a78f2650fe5.attlocal.net>
2024-11-09 17:44:16 -0800

    data formatting update, needed to handle players that got traded as well as standardize to players that played more than 41 games (majority of the season)

--- player_scraper.py
+++ player_scraper.py
@@ -1,44 +1,58 @@
 import requests
 import csv
 import time
+import re
 from bs4 import BeautifulSoup
-
 
 def main():
     url = 'https://www.basketball-reference.com/leagues/NBA_2024_totals.html'  # Update URL for 2023-2024 season
-    response = requests.get(url)
-
-    if response.status_code != 200:
-        print(f'Status code: {response.status_code}')
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Check if request was successful
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
         return
     
     content = response.text
     soup = BeautifulSoup(content, 'html.parser')
-    tds = soup.find_all('td', attrs={'data-stat': True})
+    rows = soup.find_all('tr', class_=lambda x: x != 'thead')
     
-    game_data = []
+    player_data = []
     columns = set()
-    
-    game_row = {}
-    for td in tds:
-        data_stat = td['data-stat']
-        value = td.text.strip()
+    multi_team_players = set()
+
+    for row in rows:
+        player_row = {}
+        player_name = None
+        for td in row.find_all('td', attrs={'data-stat': True}):
+            data_stat = td['data-stat']
+            value = td.text.strip()
+
+            if data_stat == 'name_display' and value:
+                player_name = value
+                if player_name in multi_team_players:
+                    break
+
+            if data_stat == 'team_name_abbr' and value and bool(re.match(r"^[2-9]TM$", value)):
+                multi_team_players.add(player_name)
+            
+            if data_stat == 'games' and value and int(value) < 41:
+                player_row = {}
+                break
+            
+            player_row[data_stat] = value
+            columns.add(data_stat)
         
-        if data_stat == 'awards' and game_row:
-            game_data.append(game_row)
-            columns.update(game_row.keys())
-            game_row = {}
-        
-        game_row[data_stat] = value
+        if player_row:
+            player_data.append(player_row)
     
     with open('player_data.csv', 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=sorted(columns))
         writer.writeheader()
-        for row in game_data:
+        for row in player_data:
             writer.writerow(row)
     
     time.sleep(3)
-
 
 if __name__ == "__main__":
     main()
